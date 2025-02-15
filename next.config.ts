06170a7e1193be108e2d2fe62b7ea0f825f46e6b
Connor Dirks <cdirks4@me.com>
2025-02-15 17:43:22 -0500

    feat: try to build

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,20 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      "use-sync-external-store/shim/with-selector": require.resolve(
+        "use-sync-external-store/shim/with-selector.js"
+      ),
+    };
+    return config;
   },
 };
 
